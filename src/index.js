59278b16d9598793f1fd8858d8a243a78b47d5a6
艸良 <lisa5566407@gmail.com>
2022-12-13 02:05:09 +0800

    <Feat : npm & webpack>

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import css from "./style.css";
+import axios from "axios";
+import bootstrap from "bootstrap";
+import gsap from "gsap";
+
+console.log('hello');
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.[hash].bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './base.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.[hash].css"
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+}
+
